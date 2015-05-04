8f46e1cf15f811b956ec3a7b636a5f82605b8995
kerneleg <atalaat@kerneleg.com>
2015-05-04 19:41:02 +0200

    Nav Upon Usertype
    Changes in Master and Webform1

--- TPA1/TPA2/Site1.Master.designer.cs
+++ TPA1/TPA2/Site1.Master.designer.cs
@@ -29,5 +29,23 @@ namespace TPA2 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// hopahopa control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl hopahopa;
+        
+        /// <summary>
+        /// Ul1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Ul1;
     }
 }
--- TPA1/TPA2/WebForm1.aspx.cs
+++ TPA1/TPA2/WebForm1.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace TPA2
@@ -11,7 +12,22 @@ namespace TPA2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.QueryString["id"] == (1).ToString())
+            {
+                ////finding the Nav associated with id 
+                HtmlGenericControl list = (HtmlGenericControl)Page.Master.FindControl("hopahopa");
 
+                ////hiding the Control
+                list.Style.Add("display", "normal");
+            }
+            if (Request.QueryString["id"] == (2).ToString())
+            {
+                ////finding the Nav associated with id 
+                HtmlGenericControl nav = (HtmlGenericControl)Page.Master.FindControl("Ul1");
+                
+                ////hiding the Control
+                nav.Style.Add("display", "normal");
+            }
         }
     }
 }
